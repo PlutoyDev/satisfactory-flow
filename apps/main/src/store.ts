4941a71cc5645efe5fa26a907d669a3da8295c87
Plutoy <plutoy.dev@outlook.com>
2024-08-02 17:40:25 +0800

    feat(main): flows atom and selected flow atom

--- apps/main/src/store.ts
+++ apps/main/src/store.ts
@@ -1,1 +1,34 @@
-//
+// Application Store using Jotai
+import { delEdges, delNodes, FlowData, getFlows, openFlowDb, setEdges, setNodes } from './db';
+import { atom } from 'jotai';
+import { Node, Edge, NodeChange, EdgeChange } from '@xyflow/react';
+
+const _flowsAtom = atom<FlowData[]>([]);
+_flowsAtom.onMount = set => void getFlows().then(set);
+export const flowsAtom = atom(get => get(_flowsAtom)); // Export the read-only atom
+
+interface SelectedFlow {
+  id: string;
+  source: 'db' | 'example';
+}
+
+const _selectedFlowAtom = atom<SelectedFlow | null>(null);
+
+export const selectedFlowAtom = atom(
+  get => get(_selectedFlowAtom),
+  (get, set, update: SelectedFlow | null) => {
+    const prev = get(_selectedFlowAtom);
+    if (prev) {
+      // TODO - Ensure changes are saved before switching flows
+    }
+    set(_selectedFlowAtom, update);
+    // TODO - Load flow from db
+    if (update) {
+      // Set URL to /{source}/{id}
+      const url = `/${update.source}/${update.id}`;
+      window.history.pushState(null, '', url);
+    } else {
+      window.history.pushState(null, '', '/');
+    }
+  },
+);
