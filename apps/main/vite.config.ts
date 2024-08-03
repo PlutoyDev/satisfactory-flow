024c8ba81fc6fdaa0a1f7e367a91a2780cbe92d2
Plutoy <plutoy.dev@outlook.com>
2024-08-03 19:26:19 +0800

    feat(main): checking for parsedDocs.json file before vite dev/build

--- apps/main/vite.config.ts
+++ apps/main/vite.config.ts
@@ -1,5 +1,11 @@
+import { stat } from 'fs/promises';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+
+stat('public/extracted/parsedDocs.json').catch(() => {
+  console.error('public/extracted/parsedDocs.json not found. Please run `pnpm parseDocs` first.');
+  process.exit(1);
+});
 
 // https://vitejs.dev/config/
 export default defineConfig({
