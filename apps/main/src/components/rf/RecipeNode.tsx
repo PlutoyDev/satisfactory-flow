b464a2e7b053e538039f436f19b80f05ecaf46fd
Plutoy <plutoy.dev@outlook.com>
2024-08-06 11:40:54 +0800

    feat(main): emulate rotation of recipe's item list

--- apps/main/src/components/rf/RecipeNode.tsx
+++ apps/main/src/components/rf/RecipeNode.tsx
@@ -44,7 +44,7 @@ export const MachineSize = {
 const defaultSize = 180;
 
 export function RecipeNode(props: NodeProps<Node<FactoryRecipeNodeData>>) {
-  const { recipeKey, clockSpeedThou = 100000 } = props.data;
+  const { recipeKey, clockSpeedThou = 100000, rotIdx = 0 } = props.data;
   const [docsMapped] = useAtom(docsMappedAtom);
 
   const recipe = recipeKey && docsMapped.recipes.get(recipeKey);
@@ -68,20 +68,48 @@ export function RecipeNode(props: NodeProps<Node<FactoryRecipeNodeData>>) {
 
   const { displayName, ingredients, products } = recipe;
   const size = MachineSize[recipe.producedIn as keyof typeof MachineSize] ?? defaultSize;
+  const swapSides = rotIdx % 2 === 1; // Left becomes top, right becomes bottom
+  const flipSides = rotIdx / 2 >= 1; // Left becomes right, top becomes bottom
 
   return (
     <FactoryNodeWrapper {...props} factoryInterfaces={res.interfaces} size={size}>
       {displayName}
-      <div className='grid grid-flow-col grid-rows-12 place-items-center gap-0.5'>
+      <div
+        className='grid place-items-center gap-0.5'
+        style={{
+          gridAutoFlow: swapSides ? 'row' : 'column',
+          [swapSides ? 'gridTemplateColumns' : 'gridTemplateRows']: 'repeat(12, minmax(0, 1fr))',
+        }}
+      >
         {[...ingredients, ...products].map(({ itemKey }, i) => {
           const { iconPath, displayName } = docsMapped.items.get(itemKey)!;
           const isIngredient = i < ingredients.length;
           const span = 12 / (isIngredient ? ingredients.length : products.length);
-          const gridRow = `span ${span} / span ${span}`;
+          const start =
+            (isIngredient
+              ? swapSides !== flipSides
+                ? ingredients.length - 1 - i
+                : i
+              : swapSides !== flipSides
+                ? products.length - 1 - (i - ingredients.length)
+                : i - ingredients.length) * span;
+
           return (
-            <Fragment key={itemKey}>
-              {i === ingredients.length && <ArrowRight className='row-span-full' />}
-              {iconPath && <img src={'/extracted/' + iconPath} alt={displayName} className='h-6 w-6' style={{ gridRow }} />}
+            <Fragment key={itemKey + i}>
+              {i === ingredients.length && (
+                <ArrowRight style={{ [swapSides ? 'gridColumn' : 'gridRow']: '1 / -1', transform: `rotate(${rotIdx * 90}deg)` }} />
+              )}
+              {iconPath && (
+                <img
+                  src={'/extracted/' + iconPath}
+                  alt={displayName}
+                  className='h-6 w-6'
+                  style={{
+                    [swapSides ? 'gridColumn' : 'gridRow']: `${start + 1} / span ${span}`,
+                    [swapSides ? 'gridRow' : 'gridColumn']: flipSides ? (isIngredient ? 3 : 1) : isIngredient ? 1 : 3,
+                  }}
+                />
+              )}
             </Fragment>
           );
         })}
