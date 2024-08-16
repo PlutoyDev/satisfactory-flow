24f879f664a698f1d0239e85c50bc06218d1ca45
Plutoy <plutoy.dev@outlook.com>
2024-08-16 09:02:02 +0800

    fix(main): misplaced edge label
    change transform and origin based on the position

--- apps/main/src/components/rf/BeltOrPipe.tsx
+++ apps/main/src/components/rf/BeltOrPipe.tsx
@@ -1,8 +1,23 @@
-import { BaseEdge, Edge, EdgeLabelRenderer, EdgeProps, getSmoothStepPath } from '@xyflow/react';
+import { CSSProperties } from 'react';
+import { BaseEdge, Edge, EdgeLabelRenderer, EdgeProps, getSmoothStepPath, Position } from '@xyflow/react';
 import { FactoryBeltOrPipeData } from '../../engines/data';
 
+const getLabelStyle = (position: Position, x: number, y: number): CSSProperties => {
+  switch (position) {
+    case 'left':
+      return { transform: `translate(${x}px, ${y}px) translate(-100%, 0)` };
+    case 'right':
+      return { transform: `translate(${x}px, ${y}px)` };
+    case 'top':
+      return { transform: `translate(${x}px, ${y}px) rotate(-90deg)`, transformOrigin: 'top left' };
+    case 'bottom':
+      return { transform: `translate(${x}px, ${y}px) rotate(90deg)`, transformOrigin: 'top left' };
+  }
+  return {};
+};
+
 export function BeltOrPipe(props: EdgeProps<Edge<FactoryBeltOrPipeData>>) {
-  const { sourceX, sourceY, targetX, targetY, selected } = props;
+  const { sourceX, sourceY, sourcePosition, targetX, targetY, targetPosition, selected } = props;
   const { startLabel, centerLabel, endLabel, colorMode, displayOnSelect } = props.data ?? {};
   const [edgePath, labelX, labelY] = getSmoothStepPath(props);
 
@@ -12,22 +27,22 @@ export function BeltOrPipe(props: EdgeProps<Edge<FactoryBeltOrPipeData>>) {
       {(!displayOnSelect || selected) && (
         <EdgeLabelRenderer>
           {!startLabel && !centerLabel && !endLabel && (
-            <p className='absolute p-2 text-xs' style={{ transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)` }}>
+            <p className='absolute p-2 text-xs' style={{ transform: `translate(${labelX}px,${labelY}px)` }}>
               <span className='loading loading-spinner'>Loading...</span>
             </p>
           )}
           {startLabel && (
-            <p className='absolute p-2 text-xs' style={{ transform: `translate(${sourceX}px, ${sourceY}px)` }}>
+            <p className='absolute p-2 text-xs transition-transform' style={getLabelStyle(sourcePosition, sourceX, sourceY)}>
               {startLabel}
             </p>
           )}
           {centerLabel && (
-            <p className='absolute p-2 text-xs' style={{ transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)` }}>
+            <p className='absolute p-2 text-xs' style={{ transform: `translate(-50%, 50%) translate(${labelX}px,${labelY}px)` }}>
               {centerLabel}
             </p>
           )}
           {endLabel && (
-            <p className='absolute p-2 text-xs' style={{ transform: `translate(-100%, -100%) translate(${targetX}px,${targetY}px)` }}>
+            <p className='absolute p-2 text-xs transition-transform' style={getLabelStyle(targetPosition, targetX, targetY)}>
               {endLabel}
             </p>
           )}
