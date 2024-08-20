d052e526876174054732eac3f5cee8bf9723ad11
Plutoy <plutoy.dev@outlook.com>
2024-08-20 10:15:00 +0800

    feat(main): jotai devtools

--- apps/main/src/App.tsx
+++ apps/main/src/App.tsx
@@ -1,9 +1,11 @@
 import { useAtom } from 'jotai';
+import { useAtomsDevtools } from 'jotai-devtools';
 import { locationAtom } from './lib/store';
 import FlowPage from './pages/Flow';
 import HomePage from './pages/Home';
 
 function App() {
+  useAtomsDevtools('store');
   const [location] = useAtom(locationAtom);
 
   switch (true) {
