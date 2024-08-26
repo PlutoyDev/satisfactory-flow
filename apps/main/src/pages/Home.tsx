97c0910d1ecb5e9f1d2889a42fdfd7c6387472f1
Plutoy <plutoy.dev@outlook.com>
2024-08-26 14:21:29 +0800

    refactor(main): separate the views into its components

--- apps/main/src/pages/Home.tsx
+++ apps/main/src/pages/Home.tsx
@@ -1,10 +1,31 @@
-import { useRef, useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 import { useAtom } from 'jotai';
 import { JSONError } from 'parse-json';
 import { ZodError } from 'zod';
 import examples from '../examples';
 import { parseFlowData } from '../lib/data';
 import { createFlow, flowsAtom, selectedFlowAtom } from '../lib/store';
+
+type View = 'create' | 'import' | 'select';
+type SetView = (view: View) => void;
+
+function HomePage() {
+  const [view, setView] = useState<View>('select');
+
+  return (
+    <div className='rounded-box absolute left-1/2 top-1/2 w-[28rem] -translate-x-1/2 -translate-y-1/2 px-8 py-4 shadow-xl bg-base-200 *:mt-2 *:*:mt-2'>
+      <h1 className='w-full text-center text-4xl font-bold'>
+        <span className='text-accent'>Satisfactory </span>
+        Flow
+      </h1>
+      {view === 'select' && <SelectView setView={setView} />}
+      {view === 'create' && <CreateView setView={setView} />}
+      {view === 'import' && <ImportView setView={setView} />}
+    </div>
+  );
+}
+
+export default HomePage;
 
 function NavigateToFlowButton({ flowName, flowId, source }: { flowName: string; flowId: string; source: 'db' | 'example' }) {
   const [, setSelectedFlow] = useAtom(selectedFlowAtom);
@@ -22,184 +43,178 @@ function NavigateToFlowButton({ flowName, flowId, source }: { flowName: string; 
   );
 }
 
-function HomePage() {
-  // const [isCreatingFlow, setCreatingFlow] = useState(false);
-  // const [isImportingFlow, setImportingFlow] = useState(false);
-  const importFileUploadRef = useRef<HTMLInputElement>(null);
-  const importJsonTextareaRef = useRef<HTMLTextAreaElement>(null);
-  const [importErrors, setImportErrors] = useState<string[]>([]);
-  const [view, setView] = useState<'create' | 'import' | 'select'>('select');
-  const [, setSelectedFlow] = useAtom(selectedFlowAtom);
+function SelectView({ setView }: { setView: SetView }) {
   const [flows] = useAtom(flowsAtom);
-
   return (
-    <div className='rounded-box absolute left-1/2 top-1/2 w-[28rem] -translate-x-1/2 -translate-y-1/2 px-8 py-4 shadow-xl bg-base-200 *:mt-2 *:*:mt-2'>
-      <h1 className='w-full text-center text-4xl font-bold'>
-        <span className='text-accent'>Satisfactory </span>
-        Flow
-      </h1>
-      {view === 'create' ? (
-        <>
-          <label htmlFor='flowName' className='form-control w-full'>
-            Flow Name:
-            <input type='text' id='flowName' className='input input-sm' />
-          </label>
-          <div className='flex justify-between w-full'>
-            <button
-              className='btn w-2/5 btn-accent'
-              onClick={() => {
-                const name = (document.getElementById('flowName') as HTMLInputElement).value;
-                createFlow(name);
-                setView('select');
-              }}
-            >
-              Create Flow
-            </button>
-            <button className='btn w-2/5 btn-error' onClick={() => setView('select')}>
-              Cancel
-            </button>
-          </div>
-        </>
-      ) : view === 'import' ? (
-        <>
-          <p className='text-lg font-bold'>Import flow</p>
-          <div className='flex flex-col gap-2'>
-            <label className='form-control'>
-              Upload File:
-              <input
-                ref={importFileUploadRef}
-                type='file'
-                id='flowFile'
-                className='file-input file-input-ghost w-full file-input-sm'
-                accept='.json'
-                onChange={e => {
-                  // Disable textarea if there is a file uploaded
-                  importJsonTextareaRef.current!.disabled = e.target.files!.length > 0;
-                }}
-              />
-            </label>
-            <label className='form-control'>
-              Raw JSON:
-              <textarea
-                ref={importJsonTextareaRef}
-                id='flowJson'
-                spellCheck='false'
-                className='textarea textarea-ghost'
-                placeholder='Paste flow JSON here'
-                onChange={e => {
-                  // Disable file upload if there is text in the textarea
-                  importFileUploadRef.current!.disabled = e.target.value.length > 0;
-                  if (importErrors.length > 0) setTimeout(() => setImportErrors([]), 4000);
-                }}
-              />
-            </label>
-          </div>
-          <ul className='list-disc list-inside text-sm text-error'>
-            {importErrors.map((error, i) => (
-              <li key={i} style={{ textIndent: error.startsWith('\t') ? '1rem' : '0' }}>
-                {error}
-              </li>
-            ))}
-          </ul>
-          <div className='flex justify-between w-full'>
-            <button
-              className='btn w-2/5 btn-accent'
-              onClick={() => {
-                const errors: string[] = [];
-                // Check if file is uploaded or JSON is pasted
-                const file = importFileUploadRef.current?.files?.[0];
-                const textareaValue = importJsonTextareaRef.current?.value;
-                if (!file && !textareaValue) {
-                  errors.push('Please upload a file or paste JSON');
-                  return;
-                } else if (file && textareaValue) {
-                  errors.push('Please upload a file or paste JSON, not both');
-                  return;
-                }
-                // Parse JSON
-                try {
-                  let flowJson: string | undefined;
-                  if (file) {
-                    const reader = new FileReader();
-                    reader.onload = () => (flowJson = reader.result as string);
-                    reader.onerror = () => (errors.push('Error reading file'), console.error(reader.error));
-                    reader.readAsText(file);
-                  } else {
-                    flowJson = textareaValue as string;
-                  }
-
-                  if (!flowJson) {
-                    throw new Error('No JSON data');
-                  }
-
-                  const flowData = parseFlowData(flowJson);
-                  setSelectedFlow({ source: 'import', flowId: flowData.info.id }, flowData);
-                } catch (e) {
-                  if (e instanceof JSONError) {
-                    if (textareaValue) {
-                      // Set cursor position to error location
-                      const lineIndex = e.message.indexOf('line') + 5;
-                      const colIndex = e.message.indexOf('column') + 7;
-                      const line = parseInt(e.message.slice(lineIndex, e.message.indexOf(' ', lineIndex)));
-                      const col = parseInt(e.message.slice(colIndex, e.message.indexOf(' ', colIndex)));
-                      let position = 0;
-                      for (let i = 1; i < line; i++) position = textareaValue.indexOf('\n', position) + 1;
-                      position += col - 1;
-                      const textarea = importJsonTextareaRef.current!;
-                      textarea.focus();
-                      textarea.setSelectionRange(position, position);
-                    }
-                    errors.push('Error Parsing imported JSON:', e.message);
-                  } else if (e instanceof ZodError) {
-                    errors.push('Invalid Flow JSON:', ...e.errors.map(e => '\t' + e.path.join('.') + ': ' + e.message));
-                  } else if (e instanceof Error) {
-                    errors.push('Error:', e.message);
-                  }
-                }
-                if (errors.length > 0) {
-                  setImportErrors(errors);
-                  return;
-                }
-              }}
-            >
-              Import Flow
-            </button>
-            <button className='btn w-2/5 btn-error' onClick={() => setView('select')}>
-              Cancel
-            </button>
-          </div>
-        </>
-      ) : (
-        <>
-          <p className='text-lg font-bold'>Select Flow</p>
-          <div className='grid grid-cols-[auto_auto] gap-2'>
-            <p className='justify-self-end'>Created: </p>
-            <div className='col-start-2 flex flex-wrap'>
-              {flows.length > 0 ? (
-                flows.map(({ id, name }) => <NavigateToFlowButton key={id} flowId={id} flowName={name} source='db' />)
-              ) : (
-                <p>No flows created yet.</p>
-              )}
-            </div>
-            <p className='justify-self-end'>Examples: </p>
-            <div className='col-start-2 flex flex-wrap'>
-              {Array.from(examples.values()).map(({ id, name }) => (
-                <NavigateToFlowButton key={id} flowId={id} flowName={name} source='example' />
-              ))}
-            </div>
-          </div>
-          <p className='text-lg'>Or create a new flow:</p>
-          <button className='btn w-full btn-outline' onClick={() => setView('create')}>
-            New Flow
-          </button>
-          <p className='text-lg'>Or import a flow:</p>
-          <button className='btn w-full btn-outline' onClick={() => setView('import')}>
-            Import Flow
-          </button>
-        </>
-      )}
-    </div>
+    <>
+      <p className='text-lg font-bold'>Select Flow</p>
+      <div className='grid grid-cols-[auto_auto] gap-2'>
+        <p className='justify-self-end'>Created: </p>
+        <div className='col-start-2 flex flex-wrap gap-1'>
+          {flows.length > 0 ? (
+            flows.map(({ id, name }) => <NavigateToFlowButton key={id} flowId={id} flowName={name} source='db' />)
+          ) : (
+            <p>No flows created yet.</p>
+          )}
+        </div>
+        <p className='justify-self-end'>Examples: </p>
+        <div className='col-start-2 flex flex-wrap'>
+          {Array.from(examples.values()).map(({ id, name }) => (
+            <NavigateToFlowButton key={id} flowId={id} flowName={name} source='example' />
+          ))}
+        </div>
+      </div>
+      <p className='text-lg'>Or create a new flow:</p>
+      <button className='btn w-full btn-outline' onClick={() => setView('create')}>
+        New Flow
+      </button>
+      <p className='text-lg'>Or import a flow:</p>
+      <button className='btn w-full btn-outline' onClick={() => setView('import')}>
+        Import Flow
+      </button>
+    </>
   );
 }
 
-export default HomePage;
+function CreateView({ setView }: { setView: SetView }) {
+  return (
+    <>
+      <label htmlFor='flowName' className='form-control w-full'>
+        Flow Name:
+        <input type='text' id='flowName' className='input input-sm' />
+      </label>
+      <div className='flex justify-between w-full'>
+        <button
+          className='btn w-2/5 btn-accent'
+          onClick={() => {
+            const name = (document.getElementById('flowName') as HTMLInputElement).value;
+            createFlow(name);
+            setView('select');
+          }}
+        >
+          Create Flow
+        </button>
+        <button className='btn w-2/5 btn-error' onClick={() => setView('select')}>
+          Cancel
+        </button>
+      </div>
+    </>
+  );
+}
+
+function ImportView({ setView }: { setView: SetView }) {
+  const importFileUploadRef = useRef<HTMLInputElement>(null);
+  const importJsonTextareaRef = useRef<HTMLTextAreaElement>(null);
+  const [importErrors, setImportErrors] = useState<string[]>([]);
+  const [, setSelectedFlow] = useAtom(selectedFlowAtom);
+
+  const onImportButtonClick = useCallback(() => {
+    const errors: string[] = [];
+    // Check if file is uploaded or JSON is pasted
+    const file = importFileUploadRef.current?.files?.[0];
+    const textareaValue = importJsonTextareaRef.current?.value;
+    if (!file && !textareaValue) {
+      errors.push('Please upload a file or paste JSON');
+      return;
+    } else if (file && textareaValue) {
+      errors.push('Please upload a file or paste JSON, not both');
+      return;
+    }
+    // Parse JSON
+    try {
+      let flowJson: string | undefined;
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = () => (flowJson = reader.result as string);
+        reader.onerror = () => (errors.push('Error reading file'), console.error(reader.error));
+        reader.readAsText(file);
+      } else {
+        flowJson = textareaValue as string;
+      }
+
+      if (!flowJson) {
+        throw new Error('No JSON data');
+      }
+
+      const flowData = parseFlowData(flowJson);
+      setSelectedFlow({ source: 'import', flowId: flowData.info.id }, flowData);
+    } catch (e) {
+      if (e instanceof JSONError) {
+        if (textareaValue) {
+          // Set cursor position to error location
+          const lineIndex = e.message.indexOf('line') + 5;
+          const colIndex = e.message.indexOf('column') + 7;
+          const line = parseInt(e.message.slice(lineIndex, e.message.indexOf(' ', lineIndex)));
+          const col = parseInt(e.message.slice(colIndex, e.message.indexOf(' ', colIndex)));
+          let position = 0;
+          for (let i = 1; i < line; i++) position = textareaValue.indexOf('\n', position) + 1;
+          position += col - 1;
+          const textarea = importJsonTextareaRef.current!;
+          textarea.focus();
+          textarea.setSelectionRange(position, position);
+        }
+        errors.push('Error Parsing imported JSON:', e.message);
+      } else if (e instanceof ZodError) {
+        errors.push('Invalid Flow JSON:', ...e.errors.map(e => '\t' + e.path.join('.') + ': ' + e.message));
+      } else if (e instanceof Error) {
+        errors.push('Error:', e.message);
+      }
+    }
+    if (errors.length > 0) {
+      setImportErrors(errors);
+      return;
+    }
+  }, [importFileUploadRef, importJsonTextareaRef, setSelectedFlow]);
+
+  return (
+    <>
+      <p className='text-lg font-bold'>Import flow</p>
+      <div className='flex flex-col gap-2'>
+        <label className='form-control'>
+          Upload File:
+          <input
+            ref={importFileUploadRef}
+            type='file'
+            id='flowFile'
+            className='file-input file-input-ghost w-full file-input-sm'
+            accept='.json'
+            onChange={e => {
+              // Disable textarea if there is a file uploaded
+              importJsonTextareaRef.current!.disabled = e.target.files!.length > 0;
+            }}
+          />
+        </label>
+        <label className='form-control'>
+          Raw JSON:
+          <textarea
+            ref={importJsonTextareaRef}
+            id='flowJson'
+            spellCheck='false'
+            className='textarea textarea-ghost'
+            placeholder='Paste exported JSON here (not meant to be hand-typed)'
+            onChange={e => {
+              // Disable file upload if there is text in the textarea
+              importFileUploadRef.current!.disabled = e.target.value.length > 0;
+              if (importErrors.length > 0) setTimeout(() => setImportErrors([]), 4000);
+            }}
+          />
+        </label>
+      </div>
+      <ul className='list-disc list-inside text-sm text-error'>
+        {importErrors.map((error, i) => (
+          <li key={i} style={{ textIndent: error.startsWith('\t') ? '1rem' : '0' }}>
+            {error}
+          </li>
+        ))}
+      </ul>
+      <div className='flex justify-between w-full'>
+        <button className='btn w-2/5 btn-accent' onClick={() => onImportButtonClick()}>
+          Import Flow
+        </button>
+        <button className='btn w-2/5 btn-error' onClick={() => setView('select')}>
+          Cancel
+        </button>
+      </div>
+    </>
+  );
+}
