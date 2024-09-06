a1c4664cb9845fc352442a9da63d951017d28248
Plutoy <plutoy.dev@outlook.com>
2024-09-06 09:26:32 +0800

    test(main): recipe node tests

--- apps/main/src/engines/itemSpeed.test.ts
+++ apps/main/src/engines/itemSpeed.test.ts
@@ -2,7 +2,12 @@ import { test, expect, describe } from 'vitest';
 import type { ExtendedNode } from '../lib/store';
 import SimpleScrew from '../tests/cases/SimpleScrew';
 import { docsMapped } from '../tests/lib';
-import { calFactoryItemSpeedForItemNode, calFactoryItemSpeedForLogisticNode, calFactoryItemSpeedForRecipeNode } from './itemSpeed';
+import {
+  calFactoryItemSpeedForItemNode,
+  calFactoryItemSpeedForRecipeNode,
+  calFactoryItemSpeedForLogisticNode,
+  ItemSpeedResult,
+} from './itemSpeed';
 
 // TODO: Move this to lib, but i'm too lazy to do it now :D
 const nodesMap = new Map<string, ExtendedNode>(SimpleScrew.nodes.map((node: ExtendedNode) => [node.id, { ...node, edges: new Map() }]));
@@ -137,5 +142,185 @@ describe('screw item node', () => {
       input: { [IRON_ROD_KEY]: 120_000 },
     });
     expect(result).toEqual({ efficiency: 0, expectedInput: { [SCREW_KEY]: 120_000 }, output: {} });
+  });
+});
+
+describe('iron rod constructor node', () => {
+  // Iron Rod recipe, clock speed is 100%
+  // 15/min ingots => 15/min rods
+  // Input test
+  test('no input provided', () => {
+    const result = calFactoryItemSpeedForRecipeNode({ node: ironRodConstructor0, docsMapped, input: {} });
+    expect(result).toEqual({
+      efficiency: 0,
+      expectedInput: { [IRON_INGOT_KEY]: 15_000 },
+      output: { ['right-solid-out-0']: { [IRON_ROD_KEY]: 0 } },
+    } satisfies ItemSpeedResult);
+  });
+
+  test('with correct input provided', () => {
+    const result = calFactoryItemSpeedForRecipeNode({
+      node: ironRodConstructor0,
+      docsMapped,
+      input: { [IRON_INGOT_KEY]: 15_000 },
+    });
+    expect(result).toEqual({
+      efficiency: 1,
+      expectedInput: { [IRON_INGOT_KEY]: 15_000 },
+      output: { ['right-solid-out-0']: { [IRON_ROD_KEY]: 15_000 } },
+    } satisfies ItemSpeedResult);
+  });
+
+  test('with lower than expected input provided', () => {
+    const result = calFactoryItemSpeedForRecipeNode({
+      node: ironRodConstructor0,
+      docsMapped,
+      input: { [IRON_INGOT_KEY]: 10_000 },
+    });
+    expect(result).toEqual({
+      efficiency: 10_000 / 15_000,
+      expectedInput: { [IRON_INGOT_KEY]: 15_000 },
+      output: { ['right-solid-out-0']: { [IRON_ROD_KEY]: 10_000 } },
+    } satisfies ItemSpeedResult);
+  });
+
+  test('with higher than expected input provided', () => {
+    const result = calFactoryItemSpeedForRecipeNode({
+      node: ironRodConstructor0,
+      docsMapped,
+      input: { [IRON_INGOT_KEY]: 20_000 },
+    });
+    expect(result).toEqual({
+      efficiency: 1,
+      expectedInput: { [IRON_INGOT_KEY]: 15_000 },
+      output: { ['right-solid-out-0']: { [IRON_ROD_KEY]: 15_000 } },
+    } satisfies ItemSpeedResult);
+  });
+
+  test('with wrong input provided', () => {
+    const result = calFactoryItemSpeedForRecipeNode({
+      node: ironRodConstructor0,
+      docsMapped,
+      input: { [IRON_ROD_KEY]: 15_000 },
+    });
+    expect(result).toEqual({
+      efficiency: 0,
+      expectedInput: { [IRON_INGOT_KEY]: 15_000 },
+      output: { ['right-solid-out-0']: { [IRON_ROD_KEY]: 0 } },
+    } satisfies ItemSpeedResult);
+  });
+
+  // Output test
+  test('expects 15/min output', () => {
+    const result = calFactoryItemSpeedForRecipeNode({
+      node: ironRodConstructor0,
+      docsMapped,
+      input: { [IRON_INGOT_KEY]: 15_000 },
+      expectedOutput: { ['right-solid-out-0']: { [IRON_ROD_KEY]: 15_000 } },
+    });
+    expect(result).toEqual({
+      efficiency: 1,
+      expectedInput: { [IRON_INGOT_KEY]: 15_000 },
+      output: { ['right-solid-out-0']: { [IRON_ROD_KEY]: 15_000 } },
+    } satisfies ItemSpeedResult);
+  });
+
+  test('expects 0 output', () => {
+    const result = calFactoryItemSpeedForRecipeNode({
+      node: ironRodConstructor0,
+      docsMapped,
+      input: { [IRON_INGOT_KEY]: 15_000 },
+      expectedOutput: {},
+    });
+    expect(result).toEqual({
+      efficiency: 0, // Efficiency gets tanked because the output is not being consumed
+      expectedInput: { [IRON_INGOT_KEY]: 0 }, // Since the output is not being consumed, the input is not being used either and will expect 0
+      output: { ['right-solid-out-0']: { [IRON_ROD_KEY]: 0 } },
+    } satisfies ItemSpeedResult);
+  });
+
+  test('expects 10/min output', () => {
+    const result = calFactoryItemSpeedForRecipeNode({
+      node: ironRodConstructor0,
+      docsMapped,
+      input: { [IRON_INGOT_KEY]: 15_000 },
+      expectedOutput: { ['right-solid-out-0']: { [IRON_ROD_KEY]: 10_000 } },
+    });
+    expect(result).toEqual({
+      efficiency: 10_000 / 15_000,
+      expectedInput: { [IRON_INGOT_KEY]: 10_000 },
+      output: { ['right-solid-out-0']: { [IRON_ROD_KEY]: 10_000 } },
+    } satisfies ItemSpeedResult);
+  });
+
+  test('expects 20/min output', () => {
+    const result = calFactoryItemSpeedForRecipeNode({
+      node: ironRodConstructor0,
+      docsMapped,
+      input: { [IRON_INGOT_KEY]: 15_000 },
+      expectedOutput: { ['right-solid-out-0']: { [IRON_ROD_KEY]: 20_000 } },
+    });
+    expect(result).toEqual({
+      efficiency: 1,
+      expectedInput: { [IRON_INGOT_KEY]: 15_000 },
+      output: { ['right-solid-out-0']: { [IRON_ROD_KEY]: 15_000 } },
+    } satisfies ItemSpeedResult);
+  });
+
+  test('not supply enough input for the expected output', () => {
+    const result = calFactoryItemSpeedForRecipeNode({
+      node: ironRodConstructor0,
+      docsMapped,
+      input: { [IRON_INGOT_KEY]: 10_000 },
+      expectedOutput: { ['right-solid-out-0']: { [IRON_ROD_KEY]: 15_000 } },
+    });
+    expect(result).toEqual({
+      efficiency: 10_000 / 15_000,
+      expectedInput: { [IRON_INGOT_KEY]: 15_000 },
+      output: { ['right-solid-out-0']: { [IRON_ROD_KEY]: 10_000 } },
+    } satisfies ItemSpeedResult);
+  });
+
+  // Iron Rod recipe (overclocked) to 200%
+  // 30/min ingots => 30/min rods
+
+  const overclockedIronRodConstructor0 = { ...ironRodConstructor0, data: { ...ironRodConstructor0.data, clockSpeedThou: 200_00_000 } };
+  test('overclocked but given 15/min input (under-provided)', () => {
+    const result = calFactoryItemSpeedForRecipeNode({
+      node: overclockedIronRodConstructor0,
+      docsMapped,
+      input: { [IRON_INGOT_KEY]: 15_000 },
+    });
+    expect(result).toEqual({
+      efficiency: 15_000 / 30_000,
+      expectedInput: { [IRON_INGOT_KEY]: 30_000 },
+      output: { ['right-solid-out-0']: { [IRON_ROD_KEY]: 15_000 } },
+    } satisfies ItemSpeedResult);
+  });
+
+  test('overclocked but given 30/min input (exact)', () => {
+    const result = calFactoryItemSpeedForRecipeNode({
+      node: overclockedIronRodConstructor0,
+      docsMapped,
+      input: { [IRON_INGOT_KEY]: 30_000 },
+    });
+    expect(result).toEqual({
+      efficiency: 1,
+      expectedInput: { [IRON_INGOT_KEY]: 30_000 },
+      output: { ['right-solid-out-0']: { [IRON_ROD_KEY]: 30_000 } },
+    } satisfies ItemSpeedResult);
+  });
+
+  test('overclocked but given 45/min input (over-provided)', () => {
+    const result = calFactoryItemSpeedForRecipeNode({
+      node: overclockedIronRodConstructor0,
+      docsMapped,
+      input: { [IRON_INGOT_KEY]: 45_000 },
+    });
+    expect(result).toEqual({
+      efficiency: 1,
+      expectedInput: { [IRON_INGOT_KEY]: 30_000 },
+      output: { ['right-solid-out-0']: { [IRON_ROD_KEY]: 30_000 } },
+    } satisfies ItemSpeedResult);
   });
 });
